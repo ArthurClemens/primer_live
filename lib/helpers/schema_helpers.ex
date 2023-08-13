897bd9c5b2fb59f381f3e818b9400b3d37acae41
Arthur Clemens <arthurclemens@gmail.com>
2023-08-13 17:36:32 +0200

    Remove unused

--- lib/helpers/schema_helpers.ex
+++ lib/helpers/schema_helpers.ex
@@ -3,41 +3,6 @@ defmodule PrimerLive.Helpers.SchemaHelpers do
 
   use Phoenix.Component
 
-  import Ecto.Changeset
-
-  @doc """
-  Validates a changeset by verifying that at least one of the keys is present.
-
-  ```
-  changeset |> validate_one_of_present(attrs, [:toggle, :menu, :option, :divider])
-  ```
-  """
-
-  def validate_one_of_present(changeset, attrs, keys) do
-    attr_keys = Map.keys(attrs)
-
-    in_list_count =
-      keys
-      |> Enum.reduce(0, fn key, acc ->
-        case Enum.member?(attr_keys, key) do
-          true -> acc + 1
-          false -> acc
-        end
-      end)
-
-    case in_list_count == 0 do
-      true ->
-        add_error(
-          changeset,
-          :dropdown_item,
-          "must contain one attribute from these options: #{keys |> Enum.join(", ")}"
-        )
-
-      false ->
-        changeset
-    end
-  end
-
   @doc """
   Validates attribute `form`.
   Allowed values:
