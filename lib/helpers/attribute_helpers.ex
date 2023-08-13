87b38dce237d283b47d303df764f9b41c641f092
Arthur Clemens <arthurclemens@gmail.com>
2023-08-13 17:54:36 +0200

    Remove Ecto dependency

--- lib/helpers/attribute_helpers.ex
+++ lib/helpers/attribute_helpers.ex
@@ -310,9 +310,28 @@ defmodule PrimerLive.Helpers.AttributeHelpers do
   end
 
   @doc """
-  Generates a random string.
+  From: https://gist.github.com/ahmadshah/8d978bbc550128cca12dd917a09ddfb7?permalink_comment_id=4178225#gistcomment-4178225
+
+  A helper functions to generate a random (printable) string of given `len`.
+  You could change the hard-coded ascii codes 32 and 126 to only
+  uppercase letters, digits, etc...
   """
-  def random_string(), do: Ecto.UUID.generate()
+  def random_string(len \\ 12) when is_integer(len) and len > 0 do
+    (for _ <- 1..len, do: rand_uniform(32, 126))
+    |> List.to_string()
+  end
+
+  # Returns a random integer uniformly distributed in the range
+  # `n <= X <= m`.
+  #
+  # If the random variable `X` is uniformly distributed in the range
+  # `1 <= X <= m - n + 1`, then r.v `Y = X + n - 1` is uniformly
+  # distributed in the range `n <= Y <= m`.
+  # (Because we just shift X to the right).
+  defp rand_uniform(n, m) do
+    :rand.uniform(m - n + 1) # random X
+    |> Kernel.+(n - 1)       # shift X to the right to get Y
+  end
 
   @doc """
   Verifies if a slot should be handled as a link.
--- mix.exs
+++ mix.exs
@@ -11,9 +11,14 @@ defmodule PrimerLive.MixProject do
       aliases: aliases(),
       name: "PrimerLive",
       deps: deps(),
-      docs: docs()
+      docs: docs(),
+      elixirc_paths: elixirc_paths(Mix.env())
     ]
   end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   defp description() do
     "A collection of function components that implements GitHub's Primer Design System."
@@ -30,11 +35,11 @@ defmodule PrimerLive.MixProject do
   defp deps do
     [
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:ecto, "~> 3.10", only: [:dev, :test], runtime: false},
-      {:ecto_sql, "~> 3.10", only: [:dev, :test], runtime: false},
+      {:ecto, "~> 3.10", only: :test, runtime: false},
+      {:ecto_sql, "~> 3.10", only: :test, runtime: false},
       {:esbuild, "~> 0.7", only: :dev},
       {:ex_doc, "~> 0.30", only: :dev},
-      {:phoenix_ecto, "~> 4.4", only: [:dev, :test], runtime: false},
+      {:phoenix_ecto, "~> 4.4", only: :test, runtime: false},
       {:phoenix_html, "~> 3.3"},
       {:phoenix_live_view, "~> 0.19"},
       {:jason, "~> 1.4"}
