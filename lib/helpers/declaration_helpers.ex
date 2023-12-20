fca674e155ca6144a06f66d758097b38df0d05e7
Arthur Clemens <arthurclemens@gmail.com>
2023-12-20 22:32:27 +0100

    Allow other classname attrs such as arrays

--- lib/helpers/declaration_helpers.ex
+++ lib/helpers/declaration_helpers.ex
@@ -3,13 +3,13 @@ defmodule PrimerLive.Helpers.DeclarationHelpers do
 
   defmacro class do
     quote do
-      attr(:class, :string, default: nil, doc: "Additional classname.")
+      attr(:class, :any, default: nil, doc: "Additional classname.")
     end
   end
 
   defmacro slot_class do
     quote do
-      attr(:class, :string, doc: "Additional classname.")
+      attr(:class, :any, doc: "Additional classname.")
     end
   end
 
