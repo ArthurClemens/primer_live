b88f86158d192988c95a67b43c4f966ed9112505
Arthur Clemens <arthurclemens@gmail.com>
2024-08-14 10:59:01 +0200

    QA

--- lib/helpers/form_helpers.ex
+++ lib/helpers/form_helpers.ex
@@ -441,21 +441,20 @@ defmodule PrimerLive.Helpers.FormHelpers do
 
   # Handle other types of changesets: Ash Framework
   def _get_ash_framework_field_errors(ash_changeset, field) do
-    cond do
-      Map.has_key?(ash_changeset.attributes, field) ->
-        ash_changeset.errors
-        |> Enum.filter(fn ash_error -> ash_error.field == field end)
-        |> Enum.map(fn ash_error ->
-          code_to_eval =
-            if Kernel.function_exported?(Ash.Error.Changes.Required, :id, 1),
-              do: "Ash.ErrorKind.message(e)",
-              else: "Exception.message(e)"
+    if Map.has_key?(ash_changeset.attributes, field) do
+      ash_changeset.errors
+      |> Enum.filter(fn ash_error -> ash_error.field == field end)
+      |> Enum.map(fn ash_error ->
+        code_to_eval =
+          if Kernel.function_exported?(Ash.Error.Changes.Required, :id, 1),
+            do: "Ash.ErrorKind.message(e)",
+            else: "Exception.message(e)"
 
-          {message, _binding} =
-            Code.eval_string(code_to_eval, [e: ash_error],
-              file: __ENV__.file,
-              line: __ENV__.line
-            )
+        {message, _binding} =
+          Code.eval_string(code_to_eval, [e: ash_error],
+            file: __ENV__.file,
+            line: __ENV__.line
+          )
 
         message
       end)
