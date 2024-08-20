984fdfac3962768ff3c81d5aa8807184d11c692d
Arthur Clemens <arthurclemens@gmail.com>
2024-08-20 23:09:39 +0200

    Cleanup

--- mix.exs
+++ mix.exs
@@ -124,8 +124,7 @@ defmodule PrimerLive.MixProject do
       "assets.build": [
         "cmd rm -rf priv/static/*",
         "cmd npm --prefix assets run build:types",
-        "cmd npm --prefix assets run build -- --format=esm --sourcemap --outfile=../priv/static/primer-live.esm.js",
-        "cmd npm --prefix assets run build -- --format=cjs --sourcemap --outfile=../priv/static/primer-live.cjs.js",
+        "cmd npm --prefix assets run build -- --bundle --format=esm --sourcemap --outfile=../priv/static/primer-live.esm.js",
         "cmd npm --prefix assets run build -- --format=iife --target=es2016 --outfile=../priv/static/primer-live.js",
         "cmd npm --prefix assets run build -- --format=iife --target=es2016 --minify --outfile=../priv/static/primer-live.min.js",
         "cmd rm -rf priv/static/*.cjs.css*",
