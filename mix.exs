4e3a5e6316c94044b555ee8487e452b7b57ab6a4
Arthur Clemens <arthurclemens@gmail.com>
2024-12-30 15:28:45 +0100

    Bump version to 0.10

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PrimerLive.MixProject do
   def project do
     [
       app: :primer_live,
-      version: "0.9.1",
+      version: "0.10.0",
       elixir: "~> 1.17",
       homepage_url: "https://github.com/ArthurClemens/primer_live",
       elixirc_paths: elixirc_paths(Mix.env()),
